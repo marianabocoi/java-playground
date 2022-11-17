4f512cde2da7f36257c3bd570d1f800412189ff3
Mariana B. (she/her/they/them) <10142989+marianabocoi@users.noreply.github.com>
2022-11-17 08:40:03 +0100

    fix some typos and add readme

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        // Write text to the standard output (terminal)
         System.out.println(saySomething());
     }
 }
