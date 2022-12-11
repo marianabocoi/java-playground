e9d3fd72bc2aa92bea62081769450f219557a7b6
Mariana B. (she/her/they/them) <10142989+marianabocoi@users.noreply.github.com>
2022-12-11 18:28:09 +0100

    Fix typo on class name

--- AmazingApplication.java
+++ AmazingApplication.java
@@ -1,4 +1,4 @@
-public class Main {
+public class AmazingApplication {
     public static void main(String[] args) {
         // Write text to the standard output (terminal)
         System.out.println("Hello Pink!");
